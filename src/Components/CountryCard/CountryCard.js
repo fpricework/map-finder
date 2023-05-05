3a7267f8f98139b782ad219f176775296914980d
Felicity Price <fpricework@gmail.com>
2023-05-05 23:30:02 +0100

    Added formatting to population output.

--- src/Components/CountryCard/CountryCard.js
+++ src/Components/CountryCard/CountryCard.js
@@ -6,7 +6,7 @@ function CountryCard({ flag, countryName, population, region, capital }) {
             <img src={flag} alt="" className="flag"/>
             <div className="country-content">
                 <h1 className="country-item" id="country-title">{countryName}</h1>
-                <p className="country-item"><span>Population:</span> {population}</p>
+                <p className="country-item"><span>Population:</span> {new Intl.NumberFormat().format(population)}</p>
                 <p className="country-item"><span>Region:</span> {region}</p>
                 <p className="country-item"><span>Capital:</span> {capital}</p>
             </div>
